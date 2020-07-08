b0aaf40ec4bf68c7c593fa0370589173015a240c
prvnkakkerla <prvnkakkerla@gmail.com>
2020-07-08 19:43:32 +0100

    addedfile

--- test/Onlineservicinglogin.js
+++ test/Onlineservicinglogin.js
@@ -3,6 +3,7 @@ describe('onlineservicnglogin' ,function(){
     it ('goestoloinpage',function(){
         browser.get("https://servicing.cashplus.com/identity/login?returnUrl=/onlineservicing/");
         browser.get();
+        element(by.id)
 
     })
 
