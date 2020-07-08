45e0622dc1f8977baf61eb448dea7ac277c2f99d
prvnkakkerla <53011305+prvnkakkerla@users.noreply.github.com>
2020-07-08 19:13:31 +0100

    first tests

---
+++ test/Onlineservicinglogin.js
@@ -0,0 +1,9 @@
+describe('onlineservicnglogin' ,function(){
+
+    it ('goestoloinpage',function(){
+        browser.get("https://servicing.cashplus.com/identity/login?returnUrl=/onlineservicing/");
+        browser.get();
+
+    })
+
+})
